7ffe746981199e64339e73fa3a174680ffd4355d
juandiego9221 <juandiego9221@gmail.com>
2019-12-27 09:11:33 -0500

    fuente base

---
+++ src/test/java/pe/com/jdmm21/reddit/app/redditclone/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.jdmm21.reddit.app.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
